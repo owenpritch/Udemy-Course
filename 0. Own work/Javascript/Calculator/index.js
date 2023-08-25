5ab1c6fff70c3a076dba0121ae755e8cf6e9e0b4
owenpritch <c2035661@newcastle.ac.uk>
2023-08-25 14:23:58 +0100

    Finished calculator

--- 0. Own work/Javascript/Calculator/index.js
+++ 0. Own work/Javascript/Calculator/index.js
@@ -16,13 +16,24 @@ function divide(num1, num2) {
 }
 
 function operation(operator, num1, num2) {
-    return operator(num1, num2)
+    return operator(num1, num2);
 }
+
+
 
 // Input arrays
 var number1Inputs = [];
+var number1;
 var number2Inputs = [];
-var operator;
+var number2;
+var operator = null;
+var answer = null;
+var display = [];
+var answer;
+var answerDisplay = null;
+const button = document.querySelectorAll("button");
+const calcDisplay = document.getElementById("display");
+calcDisplay.value = answerDisplay;
 
 
 //Event Listener Functions
@@ -30,49 +41,69 @@ function handleClick() {
     alert("Click!");
 }
 
-function handleNumber(number) {
-    alert(number);
-    //if(operator === null) {
-    //    number1Inputs.push(number);
-    //    alert(number);
-    //} else {
-    //    number2Inputs.push(number);
-    //    alert(number);
-    //}
+function handleNumber() {
+    //alert(this.value);
+    if(operator === null) {
+        number1Inputs.push(this.value);
+    } else {
+        number2Inputs.push(this.value);
+    }
+    updateDisplay(this.value);
+    
 }
 
-function handleOperator(input) {
-    alert(input);
-    //if(input === "x") {
-    //    input = "multiply";
-    //}else if (input === "/") {
-    //    input = "divide";
-    //} else if(input === "+") {
-    //    input = "add";
-    //} else if(input === "-") {
-    //    input = "subtract";
-    //}
+function handleOperator() {
+    //alert(this.value);
+    if(this.value === "*") {
+        operator = multiply;
+    }else if (this.value === "/") {
+        operator = divide;
+    } else if(this.value === "+") {
+        operator = add;
+    } else if(this.value === "-") {
+        operator = subtract;
+    }
+    updateDisplay(this.value);
 }
+
 
 function handleEquals() {
-    alert("equals");
-    //let number1 = number1Inputs.join();
-    //let number2 = number2Inputs.join();
-    //alert(operation(operator, number1, number2));
+    number1 = number1Inputs.join("");
+    number1 = Number(number1);
+    number2 = number2Inputs.join("");
+    number2 = Number(number2);
+
+    answer = operation(operator, number1, number2);
+    calcDisplay.value = answer;
 }
 
-const button = document.querySelectorAll("button");
+function handleClear() {
+    calcDisplay.value = null;
+    display = [];
+    number1Inputs = [];
+    number2Inputs = [];
+    answer = null;
+    number1 = null;
+    number2 = null;
+    operator = null;
+}
+
+function updateDisplay(item) {
+    display.push(item);
+    calcDisplay.value = display.join("");
+
+}
+
 // Assigning Event Listeners
 for(var i = 0; i < 16; i++) {
-    button[i].addEventListener("click", alert("hi"));
     if(button[i].classList.contains("number-key")) {
-        button[i].addEventListener("click", handleNumber(button[i].innerHTML));
+        button[i].addEventListener("click", handleNumber);
     } else if(button[i].classList.contains("operator")) {
-        button[i].addEventListener("click", handleOperator(button[i].innerHTML));
+        button[i].addEventListener("click", handleOperator);
     } else if(button[i].classList.contains("equals")) {
         button[i].addEventListener("click", handleEquals);
+    } else if(button[i].classList.contains("clear")) {
+        button[i].addEventListener("click", handleClear)
     }
-    ///////////////button.addEventListener("click", handleNumber(i).innerHTML);
-}
 
-//document.querySelector("1").addEventListener("click", inputs.push("1"));
+}
